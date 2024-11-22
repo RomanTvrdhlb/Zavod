a7868caea55f15e5c3b4c89e8b9c699798d910c6
Roman <romanshliakhov@gmail.com>
2024-11-22 14:21:54 +0200

    add some pages

--- source/js/_components.js
+++ source/js/_components.js
@@ -1,5 +1,6 @@
 import './components/dinamicHeight.js';
 import './components/modals.js';
+import './components/acc.js';
 import './components/mobile-menu.js';
 import './components/tabs.js';
 import './components/toTop.js';
